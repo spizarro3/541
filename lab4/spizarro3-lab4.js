8b2ff7b05e23230ad449fc50fc4ceea8bc743033
Samuel Pizarro <samuelpizarro@Samuels-MacBook-Air.local>
2024-11-17 09:40:16 -0600

    complete

--- lab4/spizarro3-lab4.js
+++ lab4/spizarro3-lab4.js
@@ -1,48 +1,48 @@
+// #1 Change the main headline
 (function () {
-    // code here
     const heroHeader = document.querySelector("h1");
-    heroHeader.innerHTML = "Supercharge Your Branch with Stellar Marketing"
+    heroHeader.innerHTML = "Supercharge Your Brand with Stellar Marketing"
 })();
-
+// #2 Change the hero headline
 (function () {
     const heroBio = document.querySelector("p");
     heroBio.innerHTML = "Leverage innovative strategies from Stellar Marketing to make your business <b><i>shine</i></b> and <b><i>succeed</i></b>";
 })();
-
+// #3 Change the image in the background
 (function (){
     let backgroundImg = `url("https://picsum.photos/id/683/1280/720")`
     let section = document.getElementById("hero");
     section.style.backgroundImage = backgroundImg;
 })();
-
+// #4 Change the background color of the nav bar
 (function (){
     const header = document.querySelector("header");
     header.style.backgroundColor = "rgb(31, 41, 55)";
 })();
-
+// #5 Remove the CTA
 (function (){
     const btn = document.querySelector('#hero a');
     btn.style.display = 'none';
 })();
-
+// #6 Change icon color
 (function (){
     const icon = document.querySelectorAll("span")
     icon.forEach( x => x.style.color = "#6495ed")
-    icon[0].textContent = `ads_click`
 })();
-
-/// this is where I left off. it's the 8th div and we want to have all the baby divs in a row
+// #7 Change icon library 
+(function (){
+    const icon = document.querySelectorAll("span");
+    icon[0].textContent = `ads_click`;
+})();
+//#8 Change display style in Specialized Marketing Solutions
 (function(){
     const div = document.querySelectorAll('div')
    
-    window.addEventListener('resize', function() {
-        
+    window.addEventListener('resize', function() { // Change display to flex or grid based off screen size;
         (window.innerWidth >= 1024) ? div[8].style.display = 'flex' : div[8].style.display = 'grid';
-        //console.log(div[8].style.display)
       });
-
 })();
-
+// #9 Change Musician's image
 (function (){
     const musicImg = "https://picsum.photos/id/453/400/300"
     const div = document.querySelectorAll('div')[12];
@@ -50,14 +50,15 @@
     const img = div.querySelector('img')
     img.src = musicImg
 })();  
-
+// Graduate Additional Requirements
 (function(){
-    const btn = document.querySelector("button");
-    btn.attributes
-    btn.style.backgroundColor = "black"
-    btn.href = "www.google.com"
-    console.log(btn)
-    btn.addEventListener("click", () =>{
-        console.log("clicked")
-    })
+    document.querySelector('form').addEventListener('submit', function(event) {
+        event.preventDefault(); // Prevent form submission
+        const name = document.getElementById("name").value;
+        const email = document.getElementById("email").value;
+      
+        if(name !== "" && email !== ""){ // if name and email have value send alert
+            alert(`Thank you, ${name}! We will be in touch with you shortly at ${email}`)
+            }else {alert('Please provide a name and email')} // else alert users to prompt inputs
+      }); 
 })();
